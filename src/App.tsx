5fe8a7b10523b48a2f35147f38dc4578a334ef6e
Angrypepee <angrypepee@gmail.com>
2024-09-26 22:06:45 +0700

    update parameter on chores, value and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // src/App.js
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [chores, setChores] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [chores, setChores] = useState<{ text: string; completed: boolean }[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
 
   //menambah chore
   const addChore = () => {
@@ -15,12 +15,12 @@ function App() {
   };
 
   //menghapus chore
-  const removeChore = (index) => {
+  const removeChore = (index:number) => {
     setChores(chores.filter((_, i) => i !== index));
   };
 
   //menampilkan chore
-  const toggleChoreCompletion = (index) => {
+  const toggleChoreCompletion = (index:number) => {
     setChores(
       chores.map((chore, i) => 
         i === index ? { ...chore, completed: !chore.completed } : chore
@@ -59,7 +59,7 @@ function App() {
                 className="text-red-500 hover:text-red-700"
               >
                 {/* remove icon */}
-                <div class="border-2 rounded p-1 border-rose-500 ...">
+                <div className="border-2 rounded p-1 border-rose-500 ...">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>

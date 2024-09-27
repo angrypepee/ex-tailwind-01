c638eb94ce7ba4e4f78771abdf7de12ee08bcaa0
Angrypepee <angrypepee@gmail.com>
2024-09-27 13:41:46 +0700

    Update : text size, color and compile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // src/App.js
-
 import { useState } from 'react';
 import banner from './assets/img/3d-casual.png' //gambar banner kosong
 import profle from './assets/img/profile.png' // gambar profile
@@ -35,68 +34,68 @@ function App() {
   const completedChores = chores.filter(chore => chore.completed).length;
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="bg-white rounded-lg shadow-lg p-4 w-full max-w-md">
 
         <div className='flex mb-3'>
-          <div>
+          <div className='items-end'>
             <img className='pr-4' src= {profle} width={70} height={70} alt='profile'/>
           </div>
-          <div>
-            <h1 className="text-xl font-bold mb-0 card">Hai, Fellas!!</h1>
-            <p>What is your plan today.</p>
-          </div>
+            <div className='items-start'>
+              <h1 className="text-xl font-bold mb-0 card text-left">Hai, Fellas!!</h1>
+              <p>What is your plan today.</p>
+            </div>
           
         </div>
 
-        <hr />
+        <hr className='mb-2'/>
 
-        
         {totalChores === 0 ? (
           <div className='flex flex-col items-center p-4'>
-            <img src= {banner} width={240} alt='banner'/>
+            <img src= {banner} width={200} alt='banner'/>
             <p className="text-gray-500 text-center mb-4">No tasks available. Please add a chore.</p>
           </div>
         ) : (
-          <ul className="list-disc m-2">
-          {chores.map((chore, index) => (
-            <li key={index} className="flex justify-between items-center mb-2">
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  checked={chore.completed}
-                  onChange={() => toggleChoreCompletion(index)}
-                  className="mr-2"
-                />
-                <div className='flex flex-col'>
-                  <span className="text-gray-400 text-xs">
-                      {chore.timestamp.toLocaleString()} {/* Format the timestamp */}
-                  </span>
-                  <span className={chore.completed ? 'line-through text-gray-500' : ''}>
-                    {chore.text}
-                  </span>
-                </div>
-                
-              </div>
-              <button
-                onClick={() => removeChore(index)}
-                className="text-red-500 hover:text-red-700"
-              >
-                {/* remove icon */}
-                <div className="border-2 rounded p-1 border-rose-500 ...">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg>
-                </div>
+            <ul className="list-disc min-h-[200px]">
+              {chores.map((chore, index) => (
+                <li key={index} className="flex justify-between items-center mb-2 border rounded pr-2 pl-2 p-1">
+                  <div className="flex items-center width-[40px]">
+                    <input
+                      type="checkbox"
+                      checked={chore.completed}
+                      onChange={() => toggleChoreCompletion(index)}
+                      className="mr-2"
+                    />
+                    <div className='flex flex-col'>
+                      <span className="text-gray-400 text-[7pt] text-base/1">
+                          {chore.timestamp.toLocaleString()} {/* Format the timestamp */}
+                      </span>
+                      <span className={chore.completed ? 'line-through text-gray-500' : ''}>
+                        {chore.text}
+                      </span>
+                    </div>
+                    
+                  </div>
+                  <button
+                    onClick={() => removeChore(index)}
+                    className="text-red-500 hover:text-red-700"
+                  >
+                    {/* remove icon */}
+                    <div className="border-2 rounded p-1 border-rose-500 ...">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                    </div>
 
-              </button>
-            </li>
-          ))}
-        </ul>
+                  </button>
+                </li>
+              ))}
+            </ul>
         )}
        
         <hr />
-        <div className="pt-4">
+
+        <div className="pt-4 items-start flex">
           <p>Add Todo</p>
         </div>
         
